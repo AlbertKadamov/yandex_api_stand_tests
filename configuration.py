5691554b1302acfb1ed77f59ec7b66393610de89
Albert <bert.kadamov@gmail.com>
2023-07-11 16:28:42 -0600

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://1b89b26d-c64f-4991-b438-cfa842045164.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCT_KITS_PATH = "/api/v1/products/kits/"
